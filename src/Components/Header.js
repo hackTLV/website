c97dd3e54b5aad64d860185a30cb331914d34d68
obivankanoobi <artium1@gmail.com>
2020-03-27 23:37:08 +0300

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,12 +28,12 @@ class Header extends Component {
     return (
       <header id="home">
         <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          {/* <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
           </a>
           <a className="mobile-btn" href="#home" title="Hide navigation">
             Hide navigation
-          </a>
+          </a> */}
 
           <ul id="nav" className="nav">
             <li className="current">
